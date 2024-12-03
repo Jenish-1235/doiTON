dec511703155a282db6d6a981e19c3153bbbc509
Jenish Togadiya <165951149+Jenish-1235@users.noreply.github.com>
2024-12-03 19:54:58 +0530

    feature-home-fragment-ui-done

---
+++ app/src/main/java/com/loopsquad/doiton/models/Task.kt
@@ -0,0 +1,8 @@
+package com.loopsquad.doiton.models
+
+object Task {
+    val taskName: String = "Task1"
+    var taskStatus: Boolean = false
+    var taskDescription: String = "Task1 Description"
+    var taskPrice: Double = 0.0
+}
